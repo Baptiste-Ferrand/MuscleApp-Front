f21b412eb8b9e40a91f0f148894c259a1a7ac7d7
kilianp07 <kilianp07@icloud.com>
2023-06-16 11:19:08 +0200

    add docker compose

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,5 +1,6 @@
 import colors from 'vuetify/es5/util/colors'
 
+
 export default {
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
@@ -43,7 +44,7 @@ export default {
   // Axios module configuration: https://go.nuxtjs.dev/config-axios
   axios: {
     // Workaround to avoid enforcing hard-coded localhost:3000: https://github.com/nuxt-community/axios-module/issues/308
-    baseURL: 'http://localhost:8080/',
+    baseURL: process.env.BASE_URL || 'http://localhost:8080/',
   },
 
   auth: {
