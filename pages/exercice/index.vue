26c19f248880fddd7766a70d4d7b8c0f88da098f
Baptiste-Ferrand <baptiste.ferrand@ynov.com>
2023-06-26 13:20:49 +0200

    Add exercice/index.vue and add player for video

---
+++ pages/exercice/index.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <div>
+      <span>Exercice Name</span>
+      <span>Description de l'exercice, quelque conseil etc</span>
+    </div>
+    <div>
+      <video-player src="https://www.youtube.com/watch?v=XMycI9c3amg" />
+    </div>
+  </div>
+</template>
+
+<script>
+import VideoPlayer from 'nuxt-video-player'
+
+require('nuxt-video-player/src/assets/css/main.css')
+export default {
+  components: { VideoPlayer },
+}
+</script>
